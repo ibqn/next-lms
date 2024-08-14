410ab1df6068b344a70cb3e900cb307d42438f23
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-08-14 21:48:20 +0200

    add update course function

--- src/apis/course.ts
+++ src/apis/course.ts
@@ -6,3 +6,14 @@ export const createCourseFn = async (courseData: CourseSchema) => {
   const response = await axios.post<NewCourse>("/api/courses", courseData)
   return response.data
 }
+
+export const updateCourseFn = async (
+  courseId: string,
+  courseData: CourseSchema
+) => {
+  const response = await axios.patch<NewCourse>(
+    `/api/courses/${courseId}`,
+    courseData
+  )
+  return response.data
+}
