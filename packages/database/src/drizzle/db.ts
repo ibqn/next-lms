a2a84f6367560a6f71145c6a4c6f1188ba43c13c
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-12-22 12:23:41 +0100

    add auth queries

--- packages/database/src/drizzle/db.ts
+++ packages/database/src/drizzle/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import { z } from "zod"
+import { sessionRelations, sessionTable, userTable } from "./schema/auth"
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
@@ -8,7 +9,11 @@ const envSchema = z.object({
 export const processEnv = envSchema.parse(process.env)
 
 export const db = drizzle(processEnv.DATABASE_URL, {
-  schema: {},
+  schema: {
+    user: userTable,
+    session: sessionTable,
+    sessionRelations,
+  },
 })
 
 const result = db.execute("select 1")
