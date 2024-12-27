88a9d63c4066fb488d7dc59b1ec1878d51c5cfa8
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-12-27 10:36:27 +0100

    fix attachment schema

--- packages/database/src/drizzle/schema/attachment.ts
+++ packages/database/src/drizzle/schema/attachment.ts
@@ -3,13 +3,17 @@ import { schema } from "./schema"
 import { courseTable } from "./course"
 import { lifecycleDates } from "./utils"
 import { relations, type InferSelectModel } from "drizzle-orm"
+import { userTable } from "./auth"
 
 export const attachmentTable = schema.table(
   "attachment",
   {
-    id: uuid("id").defaultRandom(),
+    id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull(),
     url: text("url").notNull(),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => userTable.id, { onDelete: "cascade" }),
     courseId: uuid("course_id")
       .notNull()
       .references(() => courseTable.id, { onDelete: "cascade" }),
@@ -20,6 +24,11 @@ export const attachmentTable = schema.table(
 )
 
 export const attachmentRelations = relations(attachmentTable, ({ one }) => ({
+  user: one(userTable, {
+    fields: [attachmentTable.userId],
+    references: [userTable.id],
+    relationName: "user",
+  }),
   course: one(courseTable, {
     fields: [attachmentTable.courseId],
     references: [courseTable.id],
