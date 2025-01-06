e4d8fe88b75d32ffeb9be36467279cf7d9e02ce2
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-01-06 08:17:06 +0100

    implement drag-and-drop functionality for chapter list with edit and badge support

--- packages/frontend/src/components/chapter-list.tsx
+++ packages/frontend/src/components/chapter-list.tsx
@@ -1,17 +1,79 @@
+"use client"
+
 import { Chapter } from "database/src/drizzle/schema/chapter"
+import { DragDropContext, Draggable, Droppable, DropResult } from "@hello-pangea/dnd"
+import { cn } from "@/lib/utils"
+import { GripIcon, PencilIcon } from "lucide-react"
+import { Badge } from "@/components/ui/badge"
 
 type Props = {
   chapters: Chapter[]
 }
 
 export const ChapterList = ({ chapters }: Props) => {
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return
+    }
+
+    const items = Array.from(chapters)
+    const [reorderedItem] = items.splice(result.source.index, 1)
+    items.splice(result.destination.index, 0, reorderedItem)
+  }
+  const onEdit = (chapterId: string) => () => {}
+
   return (
-    <div>
-      {chapters.map((chapter) => (
-        <div key={chapter.id}>
-          <h2>{chapter.title}</h2>
-        </div>
-      ))}
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="chapters">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {chapters.map((chapter, index) => (
+              <Draggable key={chapter.id} draggableId={chapter.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className={cn(
+                      "mb-4 flex items-center gap-x-2 rounded-md border text-sm",
+                      chapter.isPublished
+                        ? "border-sky-200 bg-sky-100 text-sky-700"
+                        : "border-slate-200 bg-slate-200 text-slate-700"
+                    )}
+                  >
+                    <div
+                      className={cn(
+                        "rounded-l-md border-r px-2 py-3 transition",
+                        chapter.isPublished
+                          ? "border-r-sky-200 hover:bg-sky-200"
+                          : "border-r-slate-200 hover:bg-slate-300"
+                      )}
+                    >
+                      <GripIcon className="size-5" />
+                    </div>
+
+                    {chapter.title}
+
+                    <div className="ml-auto flex items-center gap-x-2 pr-2">
+                      {chapter.isFree && <Badge>Free</Badge>}
+
+                      <Badge className={cn(chapter.isPublished ? "bg-sky-700" : "bg-slate-500")}>
+                        {chapter.isPublished ? "Published" : "Draft"}
+                      </Badge>
+
+                      <PencilIcon
+                        className="size-4 cursor-pointer transition hover:opacity-75"
+                        onClick={onEdit(chapter.id)}
+                      />
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
