2b81c05d93892ab4e222cc0da41e73e9608aa221
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-01-12 09:11:03 +0100

    add SingleChapterPage and ChapterPage components for chapter detail display

---
+++ packages/frontend/src/app/(dashboards)/teacher/courses/[courseId]/chapters/_components/chapter-page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { chapterQueryOptions } from "@/api/chapter"
+import { IconBadge } from "@/components/icon-badge"
+import { useSuspenseQuery } from "@tanstack/react-query"
+import { LayoutDashboardIcon } from "lucide-react"
+
+type Props = {
+  chapterId: string
+}
+
+export const ChapterPage = ({ chapterId }: Props) => {
+  const { data: chapter } = useSuspenseQuery(chapterQueryOptions(chapterId))
+
+  if (chapter === null) {
+    return <div>Not found</div>
+  }
+
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl]
+
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+  const completionStats = `(${completedFields}/${totalFields})`
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Chapter Creation</h1>
+          <span className="text-sm text-slate-700">Complete all fields {completionStats}</span>
+        </div>
+      </div>
+
+      <div className="mt-16 grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div className="space-y-4">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboardIcon} />
+              <h2 className="text-xl">Customize your chapter</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
