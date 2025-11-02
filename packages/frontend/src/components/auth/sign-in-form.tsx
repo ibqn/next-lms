8b8f9d51949707396125d29b4172e2f11ecd20a4
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-02 18:49:05 +0100

    fix(sign-in-form): simplify import statements and clean up formatting

--- packages/frontend/src/components/auth/sign-in-form.tsx
+++ packages/frontend/src/components/auth/sign-in-form.tsx
@@ -4,14 +4,7 @@ import { useForm } from "react-hook-form"
 import { CardWrapper } from "./card-wrapper"
 import { type SigninSchema, signinSchema } from "database/src/validators/signin"
 import { zodResolver } from "@hookform/resolvers/zod"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
@@ -26,9 +19,7 @@ import { toast } from "sonner"
 import { ErrorResponse, SuccessResponse } from "database/src/types"
 
 export const SignInForm = () => {
-  const [response, setResponse] = useState<
-    SuccessResponse | ErrorResponse | null
-  >(null)
+  const [response, setResponse] = useState<SuccessResponse | ErrorResponse | null>(null)
 
   const form = useForm<SigninSchema>({
     resolver: zodResolver(signinSchema),
@@ -73,11 +64,7 @@ export const SignInForm = () => {
   const isDisabled = form.formState.isSubmitting
 
   return (
-    <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/sign-up"
-    >
+    <CardWrapper headerLabel="Welcome back" backButtonLabel="Don't have an account?" backButtonHref="/sign-up">
       <Form {...form}>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div className="space-y-4">
@@ -123,9 +110,7 @@ export const SignInForm = () => {
           </div>
 
           {response?.success && <FormSuccess message={response.message} />}
-          {response?.success === false && (
-            <FormError message={response.error} />
-          )}
+          {response?.success === false && <FormError message={response.error} />}
 
           <Button type="submit" className="w-full" disabled={isDisabled}>
             Sign In
