5b1261fc98b45afd8c6b612e16ade5850c90c787
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-02 18:32:02 +0100

    fix(layout): standardize height values for Navbar and main content padding

--- packages/frontend/src/app/(dashboards)/layout.tsx
+++ packages/frontend/src/app/(dashboards)/layout.tsx
@@ -8,14 +8,14 @@ type Props = Readonly<{
 export default async function DashboardLayout({ children }: Props) {
   return (
     <div className="flex h-full grow">
-      <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-56">
+      <div className="fixed inset-y-0 z-50 h-20 w-full md:pl-56">
         <Navbar />
       </div>
 
       <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col md:flex">
         <Sidebar />
       </div>
-      <main className="flex h-full grow pt-[80px] md:pl-56">{children}</main>
+      <main className="flex h-full grow pt-20 md:pl-56">{children}</main>
     </div>
   )
 }
