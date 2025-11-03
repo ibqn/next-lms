042cd5e517c19de76fb4fadd6165416092a01a7b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 08:12:50 +0100

    refactor userId field for clarity and consistency

--- packages/database/src/drizzle/schema/stripe-customer.ts
+++ packages/database/src/drizzle/schema/stripe-customer.ts
@@ -1,12 +1,17 @@
 import { uuid } from "drizzle-orm/pg-core"
 import { schema } from "./schema"
 import { lifecycleDates } from "./utils"
+import { userTable } from "./auth"
 
 export const stripeCustomerTable = schema.table("stripe_customer", {
   id: uuid("id").primaryKey().defaultRandom(),
   stripeCustomerId: uuid("stripe_customer_id").unique(),
 
-  userId: uuid("user_id").notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => userTable.id, {
+      onDelete: "cascade",
+    }),
 
   ...lifecycleDates,
 })
