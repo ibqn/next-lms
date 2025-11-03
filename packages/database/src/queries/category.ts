063796ccb1b3eca6870e4d835249e12c54183eb4
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 09:32:31 +0100

    refactor(category): specify return type for getCategories function

--- packages/database/src/queries/category.ts
+++ packages/database/src/queries/category.ts
@@ -2,16 +2,10 @@ import { asc } from "drizzle-orm"
 import { db } from "../drizzle/db"
 import { categoryTable, type Category } from "../drizzle/schema/category"
 
-export const getCategories = async () => {
+export const getCategories = async (): Promise<Category[]> => {
   const categories = await db.query.category.findMany({
-    columns: {
-      id: true,
-      name: true,
-      createdAt: true,
-      updatedAt: true,
-    },
     orderBy: [asc(categoryTable.name)],
   })
 
-  return categories satisfies Category[] as Category[]
+  return categories satisfies Category[]
 }
