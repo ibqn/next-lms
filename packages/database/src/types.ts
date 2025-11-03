3973e2972d6fbccb264a374a02d246a2072f0b8a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 12:48:05 +0100

    refactor(types): enhance ApiResponse types for better type safety and clarity

--- packages/database/src/types.ts
+++ packages/database/src/types.ts
@@ -1,18 +1,36 @@
 export type SuccessResponse<T = void> = {
   success: true
   message: string
-} & (T extends void ? {} : { data: T })
-
-export type ErrorResponse = {
-  success: false
-  error: string
+  data: T
 }
 
-export type ApiResponse<T = void> = SuccessResponse<T> | ErrorResponse
+export type ErrorResponse<E = string> = {
+  success: false
+  error: E
+}
+
+export type ApiResponse<T = void, E = string> =
+  | SuccessResponse<T>
+  | ErrorResponse<E>
 
 export type PaginatedSuccessResponse<T> = SuccessResponse<T> & {
   pagination: {
     page: number
     totalPages: number
+    totalItems: number
   }
 }
+
+export const response = <T = void>(
+  message: string,
+  value?: T
+): SuccessResponse<T> => ({
+  success: true,
+  message,
+  data: value as T,
+})
+
+export const error = <E>(error: E): ErrorResponse<E> => ({
+  success: false,
+  error,
+})
