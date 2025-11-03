78933cd475f15c8b500aab12509862ace3283701
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 14:41:48 +0100

    feat(course): add action menu and published status badge to course columns

--- packages/frontend/src/components/columns/course.tsx
+++ packages/frontend/src/components/columns/course.tsx
@@ -3,7 +3,11 @@
 import type { ColumnDef } from "@tanstack/react-table"
 import type { Course } from "database/src/drizzle/schema/course"
 import { Button } from "@/components/ui/button"
-import { ArrowUpDown } from "lucide-react"
+import { ArrowUpDown, MoreHorizontalIcon, PencilIcon, TrashIcon } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import Link from "next/link"
+import { Badge } from "../ui/badge"
+import { cn } from "@/lib/utils"
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -36,6 +40,41 @@ export const columns: ColumnDef<Course>[] = [
           Published
           <ArrowUpDown className="ml-2 size-4" />
         </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const isPublished = row.getValue("isPublished") ?? false
+      return (
+        <Badge className={cn("bg-slate-500", isPublished && "bg-sky-700")}>{isPublished ? "Published" : "Draft"}</Badge>
+      )
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const course = row.original
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontalIcon className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem asChild>
+              <Link href={`/teacher/courses/${course.id}/`}>
+                <PencilIcon className="mr-2 size-4" />
+                Edit
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <TrashIcon className="mr-2 size-4" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )
     },
   },
