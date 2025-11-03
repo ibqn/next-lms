ceeb34bc9f70e7d9ab208b85c163f9da4f887740
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 12:50:56 +0100

    feat(course): add course retrieval and query options for pagination

--- packages/frontend/src/api/course.ts
+++ packages/frontend/src/api/course.ts
@@ -1,7 +1,11 @@
 import type { Course } from "database/src/drizzle/schema/course"
 import type { CreateCourseSchema, UpdateCourseSchema } from "database/src/validators/course"
 import { axios } from "./axios"
-import type { SuccessResponse } from "database/src/types"
+import type { ApiResponse, PaginatedSuccessResponse, SuccessResponse } from "database/src/types"
+import { paginationSchema, PaginationSchema } from "database/src/validators/pagination"
+import { keepPreviousData, queryOptions } from "@tanstack/react-query"
+import { ParamIdSchema } from "database/src/validators/param"
+import { uuid } from "zod"
 
 export const postCourse = async (courseData: CreateCourseSchema) => {
   const response = await axios.post<SuccessResponse<Course>>("/courses", courseData)
@@ -11,4 +15,42 @@ export const postCourse = async (courseData: CreateCourseSchema) => {
 export const patchCourse = async (courseId: string, courseData: UpdateCourseSchema) => {
   const response = await axios.patch<SuccessResponse<Course>>(`/courses/${courseId}`, courseData)
   return response.data
+}
+
+export const getCourseItems = async (params?: PaginationSchema) => {
+  const { data: response } = await axios.get<PaginatedSuccessResponse<Course[]>>("/courses", { params })
+  const { data: courseItems, pagination } = response
+  return { courseItems, pagination }
+}
+export type GetCourseItems = Awaited<ReturnType<typeof getCourseItems>>
+
+export const courseListQueryOptions = (paramsInput: Partial<PaginationSchema> = {}) => {
+  const params = paginationSchema.parse(paramsInput)
+
+  return queryOptions({
+    queryKey: ["course-list", params] as const,
+    queryFn: () => getCourseItems(params),
+    placeholderData: keepPreviousData,
+  })
+}
+
+export const getEventItem = async ({ id }: ParamIdSchema) => {
+  try {
+    const { data: response } = await axios.get<ApiResponse<Event>>(`/course/${id}`)
+    if (!response.success) {
+      return null
+    }
+    return response.data
+  } catch (error) {
+    console.log("Error fetching event item:", error)
+    return null
+  }
+}
+
+export const eventQueryOptions = (paramId?: ParamIdSchema) => {
+  return queryOptions({
+    queryKey: ["event", paramId?.id] as const,
+    queryFn: () => (paramId?.id ? getEventItem({ id: paramId?.id }) : null),
+    enabled: !!paramId?.id,
+  })
 }
