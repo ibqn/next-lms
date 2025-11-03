d3512c7959abda2df7b0df8554835a7ea0afa02d
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 08:16:36 +0100

    refactor(user-progress): enhance userId and chapterId fields with foreign key references

--- packages/database/src/drizzle/schema/user-progress.ts
+++ packages/database/src/drizzle/schema/user-progress.ts
@@ -2,13 +2,23 @@ import { uuid, boolean, index, unique } from "drizzle-orm/pg-core"
 import { schema } from "./schema"
 import { lifecycleDates } from "./utils"
 import { relations, type InferSelectModel } from "drizzle-orm"
+import { chapterTable } from "./chapter"
+import { userTable } from "./auth"
 
 export const userProgressTable = schema.table(
   "user_progress",
   {
     id: uuid("id").primaryKey().defaultRandom(),
-    userId: uuid("user_id").notNull(),
-    chapterId: uuid("chapter_id").notNull(),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => userTable.id, {
+        onDelete: "cascade",
+      }),
+    chapterId: uuid("chapter_id")
+      .notNull()
+      .references(() => chapterTable.id, {
+        onDelete: "cascade",
+      }),
     isCompleted: boolean("is_completed").default(false),
     ...lifecycleDates,
   },
