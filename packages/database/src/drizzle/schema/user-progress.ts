62d79c85b49f2e1ea238f01967188217fab5fa30
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-03 08:13:24 +0100

    feat(user-progress): add isCompleted field to user progress schema

--- packages/database/src/drizzle/schema/user-progress.ts
+++ packages/database/src/drizzle/schema/user-progress.ts
@@ -1,16 +1,22 @@
-import { uuid } from "drizzle-orm/pg-core"
+import { uuid, boolean, index, unique } from "drizzle-orm/pg-core"
 import { schema } from "./schema"
 import { lifecycleDates } from "./utils"
 import { relations, type InferSelectModel } from "drizzle-orm"
 
-export const userProgressTable = schema.table("user_progress", {
-  id: uuid("id").primaryKey().defaultRandom(),
-
-  userId: uuid("user_id").notNull(),
-  chapterId: uuid("chapter_id").notNull(),
-
-  ...lifecycleDates,
-})
+export const userProgressTable = schema.table(
+  "user_progress",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").notNull(),
+    chapterId: uuid("chapter_id").notNull(),
+    isCompleted: boolean("is_completed").default(false),
+    ...lifecycleDates,
+  },
+  (table) => [
+    index().on(table.chapterId),
+    unique().on(table.chapterId, table.userId),
+  ]
+)
 
 export const chapterRelations = relations(
   userProgressTable,
