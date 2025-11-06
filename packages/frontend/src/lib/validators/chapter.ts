d65d53bc29e56f007a5ab99a01e1e790dd603a46
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-06 17:25:18 +0100

    feat(validators): add videoSchema for video URL validation

--- packages/frontend/src/lib/validators/chapter.ts
+++ packages/frontend/src/lib/validators/chapter.ts
@@ -12,6 +12,11 @@ export const accessSchema = z.object({
   isFree: z.boolean(),
 })
 
+export const videoSchema = z.object({
+  videoUrl: z.string(),
+})
+
 export type TitleSchema = z.infer<typeof titleSchema>
 export type DescriptionSchema = z.infer<typeof descriptionSchema>
 export type AccessSchema = z.infer<typeof accessSchema>
+export type VideoSchema = z.infer<typeof videoSchema>
