86e58d6690f257469ea1f28b04cb355c363c1c1f
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-16 09:44:23 +0100

    feat(chapter): implement delete functionality in ChapterActions component

--- packages/frontend/src/components/chapter-actions.tsx
+++ packages/frontend/src/components/chapter-actions.tsx
@@ -4,6 +4,12 @@ import type { Chapter } from "database/src/drizzle/schema/chapter"
 import { Button } from "@/components/ui/button"
 import { Trash } from "lucide-react"
 import { ConfirmModal } from "@/components/modals/confirm-modal"
+import { useRouter } from "next/navigation"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { ParamIdSchema } from "database/src/validators/param"
+import { deleteChapter } from "@/api/chapter"
+import { toast } from "sonner"
+import { courseQueryOptions } from "@/api/course"
 
 type ChapterActionsProps = {
   chapter: Chapter
@@ -13,14 +19,41 @@ type ChapterActionsProps = {
 export const ChapterActions = ({ chapter, disabled }: ChapterActionsProps) => {
   const { isPublished } = chapter
 
+  const router = useRouter()
+
+  const queryClient = useQueryClient()
+
+  const { mutate: removeChapter, isPending } = useMutation({
+    mutationFn: (payload: ParamIdSchema) => deleteChapter(payload.id),
+    onSuccess: (data) => {
+      console.log("data:", data)
+
+      toast.success("Chapter deleted", {
+        description: `The chapter was deleted successfully`,
+      })
+
+      router.push(`/teacher/courses/${chapter.courseId}`)
+    },
+    onError: () => {
+      toast.error("Update course error", {
+        description: "Something went wrong!",
+      })
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: courseQueryOptions({ id: chapter.courseId }).queryKey,
+      })
+    },
+  })
+
   return (
     <div className="flex items-center gap-x-2">
       <Button variant="outline" size="sm" disabled={disabled}>
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
 
-      <ConfirmModal onConfirm={() => {}}>
-        <Button size="sm" disabled={disabled}>
+      <ConfirmModal onConfirm={() => removeChapter({ id: chapter.id })}>
+        <Button size="sm" disabled={isPending}>
           <Trash className="size-4" />
         </Button>
       </ConfirmModal>
