f088e47e5fb1ba4652851611fcf09414f9d46257
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-22 19:28:26 +0100

    feat: add query-string dependency and implement URL navigation in CategoryItem component

--- packages/frontend/src/components/category-item.tsx
+++ packages/frontend/src/components/category-item.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { cn } from "@/lib/utils"
 import type { Category } from "database/src/drizzle/schema/category"
 import { HashIcon } from "lucide-react"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import qs from "query-string"
 
 type CategoryItemProps = {
   category: Category
@@ -14,12 +16,35 @@ export const CategoryItem = ({ category }: CategoryItemProps) => {
   const searchParams = useSearchParams()
 
   const currentCategory = searchParams.get("category")
+  const currentTitle = searchParams.get("title")
+
+  const isSelected = currentCategory === category.name.toLowerCase()
+
+  const handleClick = () => {
+    console.log("Category clicked:", category.name)
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          category: isSelected ? null : category.name.toLowerCase(),
+          title: currentTitle,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    )
+    console.log("Navigating to URL:", url)
+    router.push(url)
+  }
 
   return (
     <button
       type="button"
-      className="flex items-center gap-x-1 rounded-full border border-slate-200 px-3 py-2 text-sm font-medium
-        text-slate-700 transition hover:border-sky-700"
+      onClick={handleClick}
+      className={cn(
+        `flex items-center gap-x-1 rounded-full border border-slate-200 px-3 py-2 text-sm font-medium text-slate-700
+        transition hover:border-sky-700`,
+        isSelected && "border-sky-700 bg-sky-200/20 text-sky-800"
+      )}
     >
       <HashIcon className="size-4 text-sky-700" />
       <span className="text-nowrap">{category.name}</span>
