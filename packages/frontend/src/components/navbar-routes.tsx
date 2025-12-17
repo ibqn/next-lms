03a870f968a32ba25139361220d4c78dd94108b9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-12-17 08:16:33 +0100

    fix: update import path for SearchInput and adjust page checks in NavbarRoutes

--- packages/frontend/src/components/navbar-routes.tsx
+++ packages/frontend/src/components/navbar-routes.tsx
@@ -4,14 +4,14 @@ import { UserButton } from "@/components/user-button"
 import { usePathname } from "next/navigation"
 import { buttonVariants } from "@/components/ui/button"
 import { LogOut } from "lucide-react"
+import { SearchInput } from "@/components/search-input"
 import Link from "next/link"
-import { SearchInput } from "./search-input"
 
 export const NavbarRoutes = () => {
   const pathname = usePathname()
 
   const isTeacherPage = pathname.startsWith("/teacher")
-  const isStudentPage = pathname.startsWith("/chapter")
+  const isCoursePage = pathname.startsWith("/course")
   const isExplorePage = pathname.startsWith("/explore")
 
   return (
@@ -19,7 +19,7 @@ export const NavbarRoutes = () => {
       {isExplorePage && <SearchInput className="hidden md:flex" />}
 
       <div className="ml-auto flex items-center gap-x-2">
-        {isTeacherPage || isStudentPage ? (
+        {isTeacherPage || isCoursePage ? (
           <Link href="/" className={buttonVariants({ variant: "ghost", size: "sm" })}>
             <LogOut className="mr-2 size-4" /> Exit
           </Link>
