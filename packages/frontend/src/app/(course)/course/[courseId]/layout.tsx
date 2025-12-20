91b99c1c150c23225b0b4fea5cb3edd850fbc040
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-12-20 13:03:19 +0100

    feat: add purchase query prefetching in CourseIdLayout for improved data handling

--- packages/frontend/src/app/(course)/course/[courseId]/layout.tsx
+++ packages/frontend/src/app/(course)/course/[courseId]/layout.tsx
@@ -1,4 +1,5 @@
 import { courseQueryOptions } from "@/api/course"
+import { purchaseQueryOptions } from "@/api/purchase"
 import { validateRequest } from "@/auth"
 import { CourseNavbar } from "@/components/course/course-navbar"
 import { CourseSidebar } from "@/components/course/course-sidebar"
@@ -19,14 +20,15 @@ export default async function CourseIdLayout({ children, params }: CourseIdLayou
 
   const { courseId } = await params
 
-  const parseResult = await paramIdSchema.safeParseAsync({ id: courseId })
-  if (!parseResult.success) {
+  const courseIdParseResult = await paramIdSchema.safeParseAsync({ id: courseId })
+  if (!courseIdParseResult.success) {
     return notFound()
   }
 
   const queryClient = getQueryClient()
 
-  await queryClient.prefetchQuery(courseQueryOptions({ id: parseResult.data.id }))
+  await queryClient.prefetchQuery(courseQueryOptions({ id: courseIdParseResult.data.id }))
+  await queryClient.prefetchQuery(purchaseQueryOptions({ id: courseIdParseResult.data.id }))
 
   return (
     <Suspense fallback="Loading...">
