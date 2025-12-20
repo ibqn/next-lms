0e1539e84b5cc71f9e8489e8df108f2a7401ec87
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-12-20 11:10:57 +0100

    feat: add pino logger middleware for enhanced logging capabilities

---
+++ packages/backend/src/middleware/pino-logger.ts
@@ -0,0 +1,11 @@
+import { pinoLogger as logger } from "hono-pino"
+import pino from "pino"
+import pretty from "pino-pretty"
+import { env } from "../env"
+
+export function pinoLogger() {
+  return logger({
+    pino: pino({ level: env.LOG_LEVEL }, pretty()),
+    http: { reqId: () => crypto.randomUUID() },
+  })
+}
