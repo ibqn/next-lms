9d9d702afe700c4f3eca9c099db5279e163e5548
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-12-21 13:42:46 +0100

    fix: update import statements for progress validation schemas to use type imports

--- packages/frontend/src/api/progress.ts
+++ packages/frontend/src/api/progress.ts
@@ -1,9 +1,9 @@
 import type { SuccessResponse } from "database/src/types"
 import { axios } from "./axios"
 import { queryOptions } from "@tanstack/react-query"
-import { paramIdSchema, ParamIdSchema } from "database/src/validators/param"
+import type { ParamIdSchema } from "database/src/validators/param"
 import type { CourseProgress } from "database/src/queries/user-progress"
-import { ProgressChapterSchema, progressChapterSchema } from "database/src/validators/chapter"
+import type { ProgressChapterSchema } from "database/src/validators/chapter"
 
 export const getUserProgress = async ({ id: courseId }: ParamIdSchema) => {
   const { data: response } = await axios.get<SuccessResponse<CourseProgress>>(`/courses/${courseId}/progress`)
