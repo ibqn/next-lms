dbe68924ea70d8ef3718a9287bf674fd8124be96
Evgeny Bobkin <evgeny.bobkin@europace.de>
2026-02-20 09:21:49 +0100

    fix: add missing line for improved code clarity in AnalyticsServerPage

--- packages/frontend/src/app/(dashboards)/editor/analytics/page.tsx
+++ packages/frontend/src/app/(dashboards)/editor/analytics/page.tsx
@@ -7,6 +7,7 @@ export default async function AnalyticsServerPage() {
   const queryClient = getQueryClient()
 
   await queryClient.prefetchQuery(editorAnalyticsQueryOptions())
+
   return (
     <Suspense>
       <AnalyticsPage />
